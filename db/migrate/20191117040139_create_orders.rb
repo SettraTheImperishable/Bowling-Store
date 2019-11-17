c5106a3c5843ba1fee233ea8c0347496f13c3130
Justin Slama <jslama@shaw.ca>
2019-11-16 22:01:49 -0600

    Added order model

---
+++ db/migrate/20191117040139_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.integer :size
+      t.decimal :subtotal, precision: 10, scale: 2
+      t.integer :sale_tax_rate
+      t.decimal :total, precision: 10, scale: 2
+      t.references :tax, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
