1efd620b1026a781d31d787a9f81f48542551fb8
Justin Slama <jslama@shaw.ca>
2019-11-18 22:41:06 -0600

    Added validations to the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,7 @@ class Product < ApplicationRecord
 
   has_many :items
   has_many :order_lines
+
+  validates :name, :description, :price, presence: true
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0 }
 end
