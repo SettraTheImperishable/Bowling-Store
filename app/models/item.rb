6421f4efca4ab6acd76aeaf3d5c9fb29e295c1da
Justin Slama <jslama@shaw.ca>
2019-11-18 21:18:36 -0600

    Added validations to the item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 class Item < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  validates :quantity, :price, presence: true
+  validates :quantity, numericiality: { only_integer: true }
 end
