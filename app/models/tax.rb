4d2c080736867281130d904a3b908a77a344b2f5
Justin Slama <jslama@shaw.ca>
2019-11-18 22:53:01 -0600

    Added validations to the tax model

--- app/models/tax.rb
+++ app/models/tax.rb
@@ -2,4 +2,7 @@
 
 class Tax < ApplicationRecord
   has_many :orders
+
+  validates :province, :rate, presence: true
+  validates :rate, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0 }
 end
