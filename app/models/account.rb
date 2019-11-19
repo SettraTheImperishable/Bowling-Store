7a7db538ee7fdfb2537906ef914c050121a0ad64
Justin Slama <jslama@shaw.ca>
2019-11-18 20:00:32 -0600

    Added validations to account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,7 @@
 class Account < ApplicationRecord
   has_one :cart
   has_many :orders
+
+  validates :email, :password, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
 end
