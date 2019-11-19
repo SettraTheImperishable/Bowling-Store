d1e07512a0bf74dd150d9123cd0ec6b833ae7a85
Justin Slama <jslama@shaw.ca>
2019-11-18 21:40:24 -0600

    Added validations to the order_line model

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
 class OrderLine < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validates :price, :quantity, presence: true
+  validates :quantity, numericality: { integer_only: true }
+  validates :price, format: { with: /\A[+-]?\d+\z/, messsage: 'Integer only. No sign allowed.' }
 end
