a3249a85cb595f59e6f9bdb64b48c423908f17fd
Justin Slama <jslama@shaw.ca>
2019-11-18 22:37:27 -0600

    Added validations to the order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,8 @@ class Order < ApplicationRecord
   belongs_to :account
 
   has_many :order_lines
+
+  validates :size, :subtotal, :sale_tax_rate, :total, presence: true
+  validates :size, numericality: { integer_only: true, greater_than: 0 }
+  validates :subtotal, :sale_tax_rate, :total, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
 end
