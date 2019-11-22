495156d9a0eb8648c784464c6094fe1d95ef9bab
Justin Slama <jslama@shaw.ca>
2019-11-21 23:10:35 -0600

    Uncommented permit params for taxes

--- app/admin/taxes.rb
+++ app/admin/taxes.rb
@@ -1,18 +1,5 @@
-ActiveAdmin.register Tax do
+# frozen_string_literal: true
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :province, :rate
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:province, :rate]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+ActiveAdmin.register Tax do
+  permit_params :province, :rate
 end
