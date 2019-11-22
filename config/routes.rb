62884139ac36f4cd58ffad286e7df5a13817bfa8
Justin Slama <jslama@shaw.ca>
2019-11-21 21:32:05 -0600

    Added resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/show'
-  get 'products/show'
-  get 'products/index'
+  resources :categories, only: %i[index show]
+
+  resources :products, only: %i[index show]
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'categories#index'
 end
